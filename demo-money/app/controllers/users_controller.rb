4da411844d4742b8bdc6c7d5867d0ca7f10285b8
Duy Chau <duychaufit@gmail.com>
2019-11-05 13:15:48 +0700

    Update users_controller.rb

--- demo-money/app/controllers/users_controller.rb
+++ demo-money/app/controllers/users_controller.rb
@@ -8,16 +8,16 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    @user.permission = 1
-    @user.del = 0
+    @user.permission = 1 // duy: magic number => use constant variable
+    @user.del = 0 // duy: default value => set default column value in database
 
     if @user.save
       flash[:success] = "Register success"
       redirect_to login_path
     else
-      flash[:success] = "Register failed"
-      puts '===>'
-      puts @user.errors[:password]
+      flash[:success] = "Register failed" // duy: flash[:error]
+      puts '===>' // duy: remove comment
+      puts @user.errors[:password]// duy: remove comment
       render :new
     end
   end
